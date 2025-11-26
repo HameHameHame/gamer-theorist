14b387c5ad40ae3f6df3adfe893bee0ca92cc1ff
Hame <45610513+HameHameHame@users.noreply.github.com>
2025-11-26 17:12:06 +0000

    refactor for rendering entities

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,50 @@ use std::io;
 use std::io::Write;
 use rand::{rng, seq::SliceRandom};
 
-fn make_grid(rows: usize, cols: usize) -> Vec<Vec<char>> {
+enum TileType {
+    Empty,
+    Grass,
+    Rock
+}
+
+struct Tile {
+    entity_id: u32,
+    tiletype: TileType
+}
+
+fn make_grid(rows: usize, cols: usize) -> Vec<Vec<Tile>> {
     let mut grid = Vec::new();   
     for _r in 0..rows {
         let mut row = Vec::new();
         for _c in 0..cols {
-            row.push('.')
+            row.push(Tile {entity_id: 0, tiletype: TileType::Grass});
         }
         grid.push(row);
     }
     return grid;
+}
+
+fn get_tile_render_char (tile: &Tile) -> char {
+    match tile.tiletype {
+        TileType::Empty => '+',
+        TileType::Grass => 'G',
+        TileType::Rock => 'R',
+    }
+}
+
+fn get_entity_render_char (tile: &Tile) -> char {
+    match tile.entity_id {
+        1 => 'A',
+        2 => 'V',
+        _ => 'E',
+    }
+}
+
+fn get_render_char (tile: &Tile) -> char{
+    if tile.entity_id == 0 {
+        return get_tile_render_char(&tile)
+    }
+    get_entity_render_char(&tile)
 }
 
 fn print_char(pixel: char) {
@@ -21,13 +55,13 @@ fn print_char(pixel: char) {
     output.write_all(f.as_bytes()).unwrap();
 }
 
-fn populate_grid(grid: &mut Vec<Vec<char>>) {
+fn populate_grid(grid: &mut Vec<Vec<Tile>>) {
     let width = grid[0].len();
     let height = grid.len();
     let total_space = height * width;
     let seeds = unique_rand_nums(total_space, 15);
     for entity in seeds {
-        grid[entity / width][entity % width] = '0';
+        grid[entity / width][entity % width].entity_id = 1;
     }
 }
 
@@ -42,10 +76,10 @@ fn unique_rand_nums(max: usize, percent: usize) -> Vec<usize> {
     nums
 }
 
-fn render_grid(grid: &Vec<Vec<char>>) {
+fn render_grid(grid: &Vec<Vec<Tile>>) {
     for row in grid {
         for value in row {
-            print_char(*value);
+            print_char(get_render_char(value));
         }
         print_char('\n');
     }
@@ -61,9 +95,9 @@ fn get_user_int() -> usize{
     get_user_input().parse().expect("not number")
 }
 
-fn place_character(grid: &mut Vec<Vec<char>>,entity: char, xpos: usize, ypos: usize) {
-    grid[xpos][ypos] = entity;
-}
+//fn place_character(grid: &mut Vec<Vec<char>>,entity: char, xpos: usize, ypos: usize) {
+//    grid[xpos][ypos] = entity;
+//}
 
 //fn get_pos () -> (usize, usize) {
 //
@@ -78,9 +112,5 @@ fn main() {
     println!("--------");
     render_grid(&user_grid);
     println!("--------");
-    place_character(&mut user_grid, 'A', rows / 2 - 1, cols / 2 - 1);
-    render_grid(&user_grid);
-    println!("col = {}", user_grid.len());
-    println!("row = {}", user_grid[0].len());
-    
+    //place_character(&mut user_grid, 'A', rows / 2 - 1, cols / 2 - 1);
 }
