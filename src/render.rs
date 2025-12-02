57a594a4c866638fe88bc62d1aa0c0f37dcaa23c
Hame <45610513+HameHameHame@users.noreply.github.com>
2025-12-02 18:49:17 +0000

    barebones renderer untested

--- src/render.rs
+++ src/render.rs
@@ -1,26 +1,47 @@
 use crate::gamespace::*;
+use crate::entity::*;
+use std::io;
+use std::io::Write;
 
 
-pub fn render_world(world: &World) {
-    for row in &world.tiles {
+pub fn render_world(gamespace: &Gamespace) {
+    for row in &gamespace.world.tiles {
         for tile in row {
-            render_tile(&tile);
+            match Some(tile.occupant) {
+                Some(_) => render_entity(&gamespace.entities[tile.occupant.unwrap()]),
+                None => render_space(&tile),
+            }
         }
         print!("\n");
     }
 }
 
-fn render_tile(tile: &Tile) {
-    match Some(tile.occupant) {
-        Some(_) => render_entity(&tile),
-        None => render_space(&tile),
+fn render_entity(entity: &Entity) {
+    print_char(get_entity_char(entity))
+}
+
+fn get_entity_char(entity: &Entity) -> char {
+    match entity.species {
+        EntityType::Hawk => 'H',
+        EntityType::Dove => 'D',
     }
 }
 
-fn render_entity() {
-
+fn render_space(tile: &Tile) {
+    print_char(get_space_char(tile))
 }
 
-fn render_space() {
-    
+fn get_space_char(tile: &Tile) -> char{
+    match tile.tiletype {
+        TileType::Empty => '+',
+        TileType::Grass => '|',
+        TileType::Rock => '=',
+    }
+}
+
+fn print_char(pixel: char) {
+    let mut output = io::stdout();
+    let mut bufr = [0; 4];
+    let f = pixel.encode_utf8(&mut bufr);
+    output.write_all(f.as_bytes()).unwrap();
 }
